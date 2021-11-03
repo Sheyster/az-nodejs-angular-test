1277c5f88d2e238218c7c4df37cd2cd0e0e74aa2
Elton <eltonlferreira@gmail.com>
2021-11-03 12:23:45 -0300

    add project

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+var bodyParser  = require('body-parser');
+const app = express(),
+      port = 8080;
+
+const users = [];
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(express.static(process.cwd()+"/my-app/dist/angular-nodejs-example/"));
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/api/user', (req, res) => {
+  const user = req.body.user;
+  users.push(user);
+  res.json("user addedd");
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(process.cwd()+"/my-app/dist/angular-nodejs-example/index.html")
+});
+
+app.listen(port, (err) => {
+   if(err) {
+     console.log(err)
+   }
+    console.log(`Server listening on the port:!!!::${port}`);
+});
